b91f82a0dbc3f640754749d157436cac56c9fd41
ezTxmMC <eztxmmc@hotmail.com>
2025-01-26 15:36:12 +0100

    uploaded base structure

---
+++ uptimer-backend/src/main/java/de/eztxm/uptimerbackend/UptimerBackendApplication.java
@@ -0,0 +1,13 @@
+package de.eztxm.uptimerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UptimerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UptimerBackendApplication.class, args);
+	}
+
+}
---
+++ uptimer-backend/src/test/java/de/eztxm/uptimerbackend/UptimerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.eztxm.uptimerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UptimerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
